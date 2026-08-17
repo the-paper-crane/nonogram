29b13eabd676b091384b289c1aaebdc69d65be37
the-paper-crane <aquaticduck11@gmail.com>
2026-08-17 20:44:14 +0100

    feat: create App window class and start mainloop

--- Nonogram.py
+++ Nonogram.py
@@ -1,3 +1,21 @@
 
 import tkinter as tk
 import customtkinter as ctk
+
+
+#
+class Window(ctk.CTk, tk.Tk):
+    def __init__(self, width, height):
+        super().__init__()
+
+        # scale
+        self.geo_width = width
+        self.geo_height = height
+
+        self.geometry(f"{self.geo_width}x{self.geo_height}")
+
+
+window = Window(600, 600)
+
+# event loop
+window.mainloop()
