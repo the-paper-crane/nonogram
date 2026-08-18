07cb9b021334304011fa7f2481f6464e2d5dda7c
the-paper-crane <aquaticduck11@gmail.com>
2026-08-18 10:50:23 +0100

    feat: add set icon method to window class

--- Nonogram.py
+++ Nonogram.py
@@ -1,5 +1,4 @@
 
-import tkinter as tk
 import customtkinter as ctk
 
 
@@ -13,6 +12,13 @@ class Window(ctk.CTk, tk.Tk):
         self.geo_height = height
 
         self.geometry(f"{self.geo_width}x{self.geo_height}")
+
+        # switch / set icon
+        self.set_icon(tk.PhotoImage(file = "/home/legolas/Downloads/GoogleIcons/CubeIcon.png"))
+        self.title("Nonogram")
+
+        def set_icon(self, icon_path):
+            self.wm_iconphoto(False, icon_path)
 
 
 window = Window(600, 600)
