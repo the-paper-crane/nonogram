067cd7a9a194b01bba9fb7184aa013df32c94109
the-paper-crane <aquaticduck11@gmail.com>
2026-08-19 13:20:41 +0100

    feat: create a Grid class for boxes to arrange and instantiate one in the window

--- Nonogram.py
+++ Nonogram.py
@@ -20,8 +20,28 @@ class Window(ctk.CTk, tk.Tk):
         def set_icon(self, icon_path):
             self.wm_iconphoto(False, icon_path)
 
+#
+class Grid(ctk.CTkFrame):
+    def __init__(self, master, width, height, radius, place_x, place_y, colour):
+
+        self.master = master
+
+        # properties
+        self.frame_width = width
+        self.frame_height = height
+        self.c_radius = radius
+        self.back_colour = colour
+
+        self.x = place_x
+        self.y = place_y
+
+        super().__init__(master, width = self.frame_width, height = self.frame_height, 
+        fg_color = self.back_colour)
+        super().place(x = self.x, y = self.y)
+
 
 window = Window(600, 600)
+grid = Grid(window, 540, 540, 2, 10, 10, '#ffffff')
 
 # event loop
 window.mainloop()
